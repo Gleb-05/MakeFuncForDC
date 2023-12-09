b9742dae20343e58d8b0fc0f4d9283440f7d44d3
GLEB <gms.4job@gmail.com>
2023-12-09 23:50:37 +0200

    Extract useful information from DC functions
    Here
    data: list of functions and corresponding terms that make them up
    info: prepare data for greedy algo by defining a "one term covers many functions" relationship

--- MakeFuncForDC.py
+++ MakeFuncForDC.py
@@ -1,24 +1,37 @@
-#===something to form (z) and (bundle)
-
 import random
 
-# greedy data
-data = {
-    "2": "3 1",
-    "3": "3 2",
-    "4": "3 2 1",
-    "7": "4 2 1",
-    "8": "4 2",
-    "9": "4 3 1",
-    "10": "4 3 1",
-    "11": "4 3 2",
-    "12": "4 3 2",
-    # 10: "1",
-    # 11: "2",
-    # 12: "2",
-    # mask is smart enough to ignore residue elements
-}
 
+# raw data
+#   function "z" : its "c" terms
+raw_z_data = {
+    "4": "5 6 7 8 9 10 11 12",
+    "3": "1 2 3 4 9 10 11 12",
+    "2": "0 3 4 7 8 11 12 15",
+    "1": "2 4 7 9 10 14",
+    }
+
+print("functions z and terms c that define them\n", raw_z_data, "\n")
+
+
+def cleaned(table):
+    return {key: val for key, val in table.items() if len(val) > 1}
+
+
+# greedy info
+#   term "c" : functions "z" it covers
+def prepare(raw_z_data):
+    data = {}
+    for z in raw_z_data:
+        for c in raw_z_data[z].split():
+            if c not in data:
+                data[c] = [z]
+            else:
+                data[c].append(z)
+    return data
+
+
+data = cleaned(prepare(raw_z_data))
+print("terms c and functions z where they are used\n", data, "\n")
 
 def make_mask(n):
     mask = []
@@ -36,23 +49,22 @@ def use_mask(word, mask):
 def make_z_intersection_table(data):
     table = {}
     for c in data:
-        z = data[c].split()
-        for mask in make_mask(len(z)):
-            mz = ' '.join(use_mask(z, mask))
+        covered_z = data[c]
+        for mask in make_mask(len(covered_z)):
+            mz = ' '.join(use_mask(covered_z, mask))
             if mz not in table:
                 table[mz] = [c]
                 continue
             table[mz].append(c)
-    return table
-
-def cleaned(table):
-    return {key: val for key, val in table.items() if len(val) > 1}
+    return cleaned(table)
 
 
+# L-ogical E-lement n-umber - count of LE needed to cover n terms
 def LEn(terms_n):
     return 1 + (terms_n - 2) // 2
 
 # greedy property
+#   density - how many terms are covered across functions at expense of how many LE
 def density(z_intersectn, bundle):
     return len(z_intersectn.split()) * len(bundle) / LEn(len(bundle))
 
@@ -61,7 +73,7 @@ def make_density_table(table):
     return {key: density(key, val) for key, val in table.items()}
 
 
-answer = {"4": [], "3": [], "2": [], "1": []}
+z_bundles = {"4": [], "3": [], "2": [], "1": []}
 
 # greedy pick
 def random_max_z_from(ro_table):
@@ -79,36 +91,34 @@ def random_max_z_from(ro_table):
 def update_data(data, used_z, bundle):
     uz = used_z.split()
     for c in bundle:
-        cz = data[c].split()
-        residue_z = [z for z in cz if z not in uz]
+        covered_z = data[c]
+        residue_z = [z for z in covered_z if z not in uz]
         if len(residue_z) < 2:
             del data[c]
         else:
-            data[c] = " ".join(residue_z)
+            data[c] = residue_z
+    data = cleaned(data)
     
-optimisedLEn = 0
+bundled_LEn = 0
 
 # greedy loop
 while True:
     if len(data) < 2:
         break
     table = make_z_intersection_table(data)
-    table = cleaned(table)
     if len(table) < 1:
         break
     ro_table = make_density_table(table)
     used_z = random_max_z_from(ro_table)
     bundle = table[used_z]
     
-    optimisedLEn += 1
+    bundled_LEn += 1
     for z in used_z.split():
-        answer[z].append(" ".join(bundle))
+        z_bundles[z].append(bundle)
     
     update_data(data, used_z, bundle)
 
 
-for func in answer:
-    print("  function", func, "used bundles:", answer[func])
-
-print("All at a cost of", optimisedLEn, "logical elements")    
-    
+for z in z_bundles:
+    print("  function", z, "used bundles:", z_bundles[z])
+print("With", bundled_LEn, "logical elements used\n")    
