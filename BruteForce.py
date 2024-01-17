7df81e7517a4adccdfb3b65471f81e3b49e078d3
GLEB <gms.4job@gmail.com>
2024-01-17 20:13:32 +0200

    Encapsute full loop in a method to find all possible covers for a function

--- BruteForce.py
+++ BruteForce.py
@@ -1,3 +1,5 @@
+from itertools import combinations_with_replacement
+
 # https://stackoverflow.com/questions/12836385/how-can-i-interleave-or-create-unique-permutations-of-two-strings-without-recur/12837695#12837695
 def unique_permutations(seq):
     i_indices = list(range(len(seq) - 1, -1, -1))
@@ -24,28 +26,6 @@ def unique_permutations(seq):
         (seq[k], seq[i]) = (seq[i], seq[k])
         seq[k + 1:] = seq[-1:k:-1]
         
-#print("check, unique permutations of 2,3,3:", unique_permutations([2,3,3]))
-
-raw_z_func = "5 6 7 8 9 10 11 12".split()
-
-m = len(raw_z_func)
-k = 3
-
-def LEn_k(terms_n, k):
-    return 1 + (terms_n - 2) // (k - 1)
-
-
-# https://stackoverflow.com/questions/26812803/find-all-possible-combinations-partitions-of-2-numbers-for-a-given-number
-from itertools import combinations_with_replacement
-
-LEk_configs = {}
-# range from best(k'=k) to worst(k'=2) possible number of LE, LE used to cover function
-for leN in range(LEn_k(m,k), LEn_k(m,2)+1):
-    LEk_configs[leN] = [list(x) for x in combinations_with_replacement(range(1,k), leN) if sum(x) == m-1]
-
-print("number of used up LEs and corresponding configurations for their inputs: ", LEk_configs)
-
-
 def define_LE_positions_for_(one_config_permutation, raw_z_func):
     permutation = [x+1 for x in one_config_permutation]
     print("selected permutation of k configuration")
@@ -84,15 +64,36 @@ def define_LE_positions_for_(one_config_permutation, raw_z_func):
         print(cover)
     print()
     
+k = 3
 
-for leN_group in LEk_configs:
-    for k_config in LEk_configs[leN_group]:
-        k_config_permutations = unique_permutations(k_config)
+def define_func_covers(raw_z_func):
+    def LEn_k(terms_n, k):
+        return 1 + (terms_n - 2) // (k - 1)
+
+    m = len(raw_z_func)
+
+    LEk_configs = {}
+
+    # https://stackoverflow.com/questions/26812803/find-all-possible-combinations-partitions-of-2-numbers-for-a-given-number
+    # possible number of LE ranges from best(k'=k) to worst(k'=2), LE are used up to cover function
+    for leN in range(LEn_k(m,k), LEn_k(m,2)+1):
+        LEk_configs[leN] = [list(x) for x in combinations_with_replacement(range(1,k), leN) if sum(x) == m-1]
+
+    print("number of used up LEs and corresponding configurations for their inputs: ", LEk_configs)
+
+    for leN_group in LEk_configs:
+        for k_config in LEk_configs[leN_group]:
+            k_config_permutations = unique_permutations(k_config)
         
-        print(f"permutations for configuration with {leN_group} LEs:")
-        for config in k_config_permutations:
-            print(config)
-        print()
+            print(f"permutations for configuration with {leN_group} LEs:")
+            for config in k_config_permutations:
+                print(config)
+            print()
          
-        for one_config_permutation in k_config_permutations:
-            define_LE_positions_for_(one_config_permutation, raw_z_func)
+            for one_config_permutation in k_config_permutations:
+                define_LE_positions_for_(one_config_permutation, raw_z_func)
+            
+
+raw_z_func = "5 6 7 8 9 10 11 12".split()
+define_func_covers(raw_z_func)
+
