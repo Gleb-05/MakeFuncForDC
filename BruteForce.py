cee747e363618ec006ac89a920e328b2ddc512e6
GLEB <gms.4job@gmail.com>
2024-01-18 14:13:04 +0200

    Separate cover signature from nested array construction

--- BruteForce.py
+++ BruteForce.py
@@ -27,69 +27,78 @@ def unique_permutations(seq):
         (seq[k], seq[i]) = (seq[i], seq[k])
         seq[k + 1:] = seq[-1:k:-1]
 
-template_debug = False
+k = 3
+signatures_debug = False
 
-def substitute_values(nested_list, values):
-    index = 0
 
-    def substitute_recursively(lst):
-        nonlocal index
-        result = []
-        for item in lst:
-            if isinstance(item, list):
-                result.append(substitute_recursively(item))
-            else:
-                result.append(values[index])
-                index += 1
-        return result
-
-    return substitute_recursively(nested_list)
-
-def define_LE_positions_for_(one_config_permutation, z_func):
-    permutation = [x+1 for x in one_config_permutation]
-    if template_debug:
+def signatures_for(permutation, m):
+    if signatures_debug:
         print("selected permutation of k configuration")
         print(permutation)
 
     init_k = permutation[0]
-    func_covers = [[z_func[:init_k]] + z_func[init_k:]]
+    signatures = [[init_k]]
+    term_counts = [m - init_k]
 
     for ki in range(1,len(permutation)):
         k = permutation[ki]
     
         at = 1
-        i_at = (len(func_covers) + 1) // 2
+        i_at = (len(signatures) + 1) // 2
     
-        for i in range(len(func_covers)):
-            cover = func_covers[i]
+        for i in range(len(signatures)):
+            signature = signatures[i]
+            termCount = term_counts[i]
         
             if ki == len(permutation) - 1:
-                if len(cover) != k:
-                    print("UNEXPECTED number of terms in", cover)
-                break
+                signatures[i] = signature + ["j", k]
+                continue
         
-            # 'stay' is 0
-            func_covers[i] = [cover[:k]] + cover[k:]
-            # 'stay' is 1
+            # 'jump' is 1 ('stay' is 0)
             if (i == i_at):
                 i_at += (i_at + 1) // 2
-                at += 1
+                at += 1   
+            term_counts[i] = termCount + at - k
+
+            signatures[i] = signature + ["j", k]
+             
+            # 'jump' is 0
             # ignore cover if k implies terms out of bounds
-            if (at+k <= len(cover)):
-                func_covers.append(cover[:at] + [cover[at:at+k]] + cover[at+k:])
+            if (termCount - k >= 0):
+                signatures.append(signature + [k])
+                term_counts.append(termCount - k)
 
-    if template_debug:
-        print("all possible LE positions for selected permutation of one configuration")
-        for cover in func_covers:
-            print(cover)
-        print()
-    return func_covers
+    if signatures_debug:
+        print("all possible signatures for selected permutation of one configuration")
+        for signature in signatures:
+            print(signature)
+        print()    
+        
+    return signatures
 
 
-k = 3
+def get_cover_by(signature, values):
+    at = 0
+    for ki in range(len(signature)):
+        k = signature[ki]
+        
+        if ki == len(signature) - 1:
+            if len(values) != k:
+                print("UNEXPECTED number of terms in", values)
+            break
+        if k == 'j':
+            at = 0
+        else:
+            values = values[:at] + [values[at:at+k]] + values[at+k:]
+            at += 1
+    return values
 
+# for signature in signatures_for([3,3,3,2], 8):
+#     print(signature)
+#     print(get_cover_by(signature, [x for x in range(8)]))
+    
 
-def define_template_covers(m):
+def define_cover_signatures(m):
     def LEn_k(terms_n, k):
         return 1 + (terms_n - 2) // (k - 1)
 
@@ -104,44 +113,59 @@ def define_template_covers(m):
     for leN_group in LEk_configs:
         print(leN_group, ":", LEk_configs[leN_group])
 
-    template_covers = []
-    
+    cover_signatures = []
+
     for leN_group in LEk_configs:
         for k_config in LEk_configs[leN_group]:
             k_config_permutations = unique_permutations(k_config)
             
-            if template_debug:
+            if signatures_debug:
                 print(f"permutations for configuration with {leN_group} LEs:")
                 for config in k_config_permutations:
                     print(config)
                 print()
          
             for one_config_permutation in k_config_permutations:
-                covers_for_permutation = define_LE_positions_for_(one_config_permutation, [c for c in range(m)])
-                template_covers.extend(covers_for_permutation)
+                permutation = [x+1 for x in one_config_permutation]
+                # TOO COMPLEX CHECK
+                # for signature in signatures_for_permutation:
+                #     is_unique = True
+                #     for signature_permutation in signature_permutations(signature):
+                #         if signature_permutation in cover_signatures:
+                #             is_unique = False
+                #             break
+                #     if is_unique:
+                cover_signatures.extend(signatures_for(permutation, m))
     
-    return template_covers
+    return cover_signatures
 
+# for cover in define_cover_signatures(8):
+#     print(cover)
+    
 
 def define_func_covers(raw_z_func):
     print(f"Definition for ALL covers for {raw_z_func} function STARTED")
     m = len(raw_z_func)
     print("Expected", math.factorial(m), "to be done")
-    template_covers = define_template_covers(m)
-    
+    cover_signatures = define_cover_signatures(m)
+
     func_covers = []
 
     active_counter = 0
-    # for all unique permutations, substitute values in template covers
+    # for all unique permutations, construct covers by signatures
     for constituent_permutation in unique_permutations(raw_z_func):
         active_counter += 1
         if active_counter%1000 == 0:
             print("Done", active_counter)
-        func_covers.extend([substitute_values(template, constituent_permutation) for template in template_covers])
+        func_covers.extend([get_cover_by(signature, constituent_permutation) for signature in cover_signatures])
     
     print("Actual", active_counter, "done")
     return func_covers
     
-        
-raw_z_func = "5 6 7 8 9 10 11 12".split()
-define_func_covers(raw_z_func)
+
+
+
+z4_covers = define_func_covers("5 6 7 8 9 10 11 12".split())
+# z3_covers = define_func_covers("1 2 3 4 9 10 11 12".split())
+# z2_covers = define_func_covers("0 3 4 7 8 11 12 15".split())
+# z1_covers = define_func_covers("2 4 7 9 10 14".split())
